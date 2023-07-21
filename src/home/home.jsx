5aeb7df6b02b86e28c1403362bc4c43676797cca
GaganDamoriya <gaganprakash123456@gmail.com>
2023-07-22 01:57:19 +0530

    error2resolved

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./style.scss";
 import HeroBanner from "./heroBanner/HeroBanner";
 import Popular from "./popular/Popular";
-import Trending from "./trending/trending";
+import Trending from "./trending/Trending";
 import TopRated from "./topRated/TopRated";
 
 const Home = () => {
